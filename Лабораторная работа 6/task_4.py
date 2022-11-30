21d89222a7a1724b7b94baf09edc90a1cf8a5403
kuleshella <115474011+kuleshella@users.noreply.github.com>
2022-11-30 22:38:39 +0300

    fix task 4

--- Лабораторная работа 6/task_4.py
+++ Лабораторная работа 6/task_4.py
@@ -4,13 +4,13 @@ from pathlib import Path
 INPUT_FILE = "input.csv"
 
 
-def csv_to_list_dict(filename: str, delimiter=',', new_line='\n') -> list[dict]:
-    def record_to_dict(column_names: list[str], record: list[str]) -> dict:
-        return {column_names[i]: record[i] for i in range(len(column_names))}
-
-    csv_lines = Path(filename).read_text().split(new_line)
-    column_names = csv_lines[0].split(delimiter)
-    return [record_to_dict(column_names, line.split(delimiter)) for line in csv_lines[1:]]
+def csv_to_list_dict(filename: str, delimiter=',', newline='\n') -> list[dict]:
+    with open(filename, mode='r', newline=newline) as output:
+        headers = next(output).rstrip(newline).split(delimiter)
+        return [
+            {headers[n]: it.rstrip(newline) for n, it in enumerate(line.rstrip(newline).split(delimiter))}
+            for line in output
+        ]
 
 
 print(json.dumps(csv_to_list_dict(INPUT_FILE), indent=4))
